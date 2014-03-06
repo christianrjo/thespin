c3c7fd7c2fa62484002beea8ba1258de56b05db8
christianrjo <christianrjo@gmail.com>
2014-03-06 09:50:18 -0800

    magic number no longer

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,8 +1,9 @@
 module Parser
   extend self
+  EXPECTED_LENGTH = 3
   def parse(row)
     pieces = row.split(';')
-    if pieces.length == 3
+    if pieces.length == EXPECTED_LENGTH
       pieces.pop.split(' ').each_slice(pieces.pop.to_i).to_a
     else
       nil
